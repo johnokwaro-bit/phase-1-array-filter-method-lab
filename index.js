2c07bfb16983b938d029d76e461e1368367f710a
John Okwaro <john.okwaro@moringaschool.com>
2022-08-22 00:39:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution here
+const collection = [];
+
+function findMatching(collection, driverName){
+    const matchingNames = [];
+    for(const driver of collection){
+        if(driver.toUpperCase() === driverName.toUpperCase()){
+            matchingNames.push(driver);
+        }        
+    }
+    return matchingNames;
+    
+}
+
+function fuzzyMatch(collection, driverName){
+    const nameThatMatch = [];
+    for (const driver of collection){
+        if (driver[0] === driverName[0]){
+            nameThatMatch.push(driver);
+        }
+    }
+    return nameThatMatch;
+    
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function matchName(drivers, driverName){
+    const elementMatch = []
+    for(const driver of drivers){
+        if (driver.name === driverName){
+            elementMatch.push(driver);
+        }
+    }
+    return elementMatch;
+}
